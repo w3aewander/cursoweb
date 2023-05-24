e730e3f28b4d4f0b5ea03c4f2e4d2dbcec3d1dbc
w3aewander <wander.silva@gmail.com>
2023-05-23 23:59:36 -0300

    Tornando app SPA estática

--- js/app.js
+++ js/app.js
@@ -1,11 +1,21 @@
 document.addEventListener('DOMContentLoaded', (evt) => {
 
-    // window.addEventListener("scroll", function () {
-    //     const cards = document.querySelector("div");
-    //     cards.forEach(element => {
-    //         element.scrollIntoView({ behavior: "smooth" });
-    //     });
-    // });
+    window.addEventListener("scroll", function () {
+        var content = document.getElementById('body');
+        var contentTop = content.offsetTop;
+        var contentHeight = content.offsetHeight;
+        var windowHeight = window.innerHeight;
+        var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        
+        var scrollPosition = scrollTop + windowHeight;
+        
+        if (scrollPosition > contentTop && scrollTop < contentTop + contentHeight) {
+          content.classList.remove('fade-out');
+        } else {
+          content.classList.add('fade-out');
+        }
+    });
+     
 // Selecione todos os elementos âncora que têm um atributo "href" iniciado com "#" (para destinos internos)
 const elementosAncora = document.querySelectorAll('a[href^="#"]');
 
@@ -14,20 +24,35 @@ elementosAncora.forEach(function(elemento) {
   elemento.addEventListener("click", function(event) {
     event.preventDefault(); // Impede o comportamento padrão do link
 
-    document.body.style.visibility = 'hidden';
+    
 
     const destino = document.querySelector(this.getAttribute("href"));
 
     if (destino) {
-      destino.scrollIntoView({ behavior: "smooth" });
-      
-      setTimeout( ()=> {document.body.style.visibility = 'visible'},500 )
+        
+    executarSeq(); 
+    
     }
 
+    async function executarSeq() {
+        await irParaDestino();
+        await tornarBodyVisivel();
+    };
 
-    
+    async function irParaDestino() {
+        document.body.style.visibility = 'hidden';
+        setTimeout( () => destino.scrollIntoView({ behavior: "smooth" }), 100);
+
+    }
+
+    async function tornarBodyVisivel(){
+        setTimeout( () => {
+        document.body.style.visibility = 'visible';
+        }, 490 );
+    }
 
   });
+
 });
 
 
@@ -37,3 +62,8 @@ elementosAncora.forEach(function(elemento) {
 function subir() {
     scrollTo(0, 0);
 }
+
+function toggleBodyVisibility() {
+    var body = document.querySelector('body');
+    body.classList.toggle('visible');
+  }
